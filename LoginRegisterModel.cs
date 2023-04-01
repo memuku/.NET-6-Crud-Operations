4df1bbca8e4aa334c46e539d5eb853d7441b1285
memuku <119692232+memuku@users.noreply.github.com>
2023-04-01 23:33:27 +0300

    Add files via upload

---
+++ LoginRegisterModel.cs
@@ -0,0 +1,12 @@
+﻿public class LoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class RegisterModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
