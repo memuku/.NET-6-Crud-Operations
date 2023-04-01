ba1598cb114f9e53a97ba61360bb3d322c754dd1
memuku <119692232+memuku@users.noreply.github.com>
2023-04-01 23:33:53 +0300

    Add files via upload

---
+++ IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Dot6.MongoDb.API.CRUD.Collections;
+namespace Dot6.MongoDb.API.CRUD.Repository;
+
+public interface IUserService
+{
+    Task<List<User>> GetAllAsync();
+    Task<User> GetByIdAsync(string id);
+    Task CreateNewUserAsync(User newUser);
+    Task UpdateUserAsync(User userToUpdate);
+    Task DeleteUserAsync(string id);
+    Task<User> Login(LoginModel model);
+    Task<User> Register(RegisterModel model);
+}
