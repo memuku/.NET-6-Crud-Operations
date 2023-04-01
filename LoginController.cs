bc14a0ea2b79a3f927f311b14c1f40ceff3155e5
memuku <119692232+memuku@users.noreply.github.com>
2023-04-01 23:32:49 +0300

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Dot6.MongoDb.API.CRUD.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dot6.MongoDb.API.CRUD.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public LoginController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
+    {
+        // Login işlemleri
+        var user = await _userService.Login(model);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+        return Ok(user);
+    }
+}
+
