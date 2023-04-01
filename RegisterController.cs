c312c05ec89b98965b145e813ac5b2b62432d6e3
memuku <119692232+memuku@users.noreply.github.com>
2023-04-01 23:33:10 +0300

    Add files via upload

---
+++ RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Dot6.MongoDb.API.CRUD.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace RegisterController.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public RegisterController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            // Register işlemleri
+            var user = await _userService.Register(model);
+            return Created("", user);
+        }
+    }
+}
